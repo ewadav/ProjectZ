19c88ea926081b6b6a94d07d5147d8432650ca45
David Ewald <ewadav@uw.edu>
2013-03-24 05:04:01 -0700

    sprites

---
+++ Project Z/src/Main.java
@@ -0,0 +1,43 @@
+
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Main extends BasicGame {
+	
+
+	
+	public Main()  {
+		super("Project Z");
+	}
+
+	
+	@Override
+	public void init(GameContainer container) throws SlickException  {
+	
+	}
+
+	
+	@Override
+	public void update (GameContainer gc, int delta) throws SlickException  {
+		
+	}
+
+	
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		
+	}
+
+	
+	public static void main(String[] args) throws SlickException {
+		
+
+	}
+
+}
